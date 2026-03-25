12a0f4c99b7ed349d8316556dfe7cea04e524104
Cornbread2100 <cornbread2100@cornbread2100.com>
2026-03-24 22:08:58 -0400

    api/user

---
+++ api/user.js
@@ -0,0 +1,29 @@
+export default {
+    name: 'checkuser',
+    description: 'Gets information about a user',
+    options: [
+        {
+            name: 'id',
+            description: 'the id of the user to get information about',
+            required: true
+        }
+    ],
+    async execute({ config, res, url, supabase, tagList }) {
+        let id = url.searchParams.get('id');
+        
+        let { data: account, error } = await supabase.from(config.supabase.tables.users).select('*').eq('id', id).limit(1);
+        if (error) {
+            res.statusCode = 500;
+            res.end(JSON.stringify({ error: 'Error fetching user', details: error.message }));
+            return;
+        }
+        account = account[0];
+        if (error) {
+            res.statusCode = 400;
+            res.end(JSON.stringify({ error: `No registered user with id "${id}" could be found` }));
+            return;
+        }
+
+        res.end(JSON.stringify(account));
+    }
+}
