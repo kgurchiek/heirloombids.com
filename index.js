c2be7d358197cfaefc51b95e5019e6b8a58dd3f7
Cornbread2100 <cornbread2100@cornbread2100.com>
2026-04-13 21:20:42 -0400

    http mode

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import fs from 'node:fs';
 import util from 'node:util';
+import http from 'node:http';
 import http2 from 'node:http2';
 import crypto from 'node:crypto';
 import readline from 'node:readline';
@@ -7,13 +8,14 @@ import cookie from 'cookie';
 import { config, client, getUser, updateCache, supabaseCache } from './lib.js';
 import { Events } from 'discord.js';
 
-let authUrl = new URL(`https://${config.web.hostname}`);
+const origin = `${config.web.https ? 'https' : 'http'}://${config.web.hostname}`;
+
+let authUrl = new URL(origin);
 authUrl.pathname = '/auth';
 let oauthUrl = new URL(config.web.oauth);
 oauthUrl.searchParams.set('redirect_uri', authUrl.href);
-let registerUrl = new URL(`https://${config.web.hostname}`);
+let registerUrl = new URL(origin);
 registerUrl.pathname = '/register';
-const favicon = await fs.promises.readFile('static/favicon.ico');
 const errorPages = {};
 await Promise.all((await fs.promises.readdir('error')).map(async a => errorPages[a.slice(0, a.lastIndexOf('.'))] = (await fs.promises.readFile(`error/${a}`)).toString()));
 let count = Object.keys(errorPages).length;
@@ -98,12 +100,6 @@ client.once(Events.ClientReady, async () => {
 });
 client.login(config.discord.token);
 
-let server = http2.createSecureServer({
-    key: fs.readFileSync(config.web.privateKey),
-    cert: fs.readFileSync(config.web.certificate)
-});
-server.on('error', (err) => console.error(err));
-
 let requestQueue = [];
 async function handleRequestQueue() {
     let queue = requestQueue.splice(0);
@@ -112,155 +108,160 @@ async function handleRequestQueue() {
 }
 handleRequestQueue();
 
-server.on('request', (req, res) => requestQueue.push(async () => {
-    let url = new URL(`https://localhost${req.url}`);
-    url.hostname = config.web.hostname;
+function handleRequest(req, res) {
+    requestQueue.push(async () => {
+        let url = new URL(origin);
+        url.pathname = req.url;
 
-    function end(code, body) {
-        if (code != null) res.statusCode = code;
-        res.end(typeof body == 'object' ? JSON.stringify(body) : body);
-    }
-
-    function redirect(location) {
-        res.setHeader('Location', location);
-        end(303);
-    }
-
-    function authRedirect(state, location) {
-        state = state || url.href;
-        location = location || oauthUrl.href;
-        let redirectUrl = new URL(location);
-        redirectUrl.searchParams.set('state', state);
-        redirect(redirectUrl.href);
-    }
-
-    try {
-        let cookies = cookie.parseCookie(req.headers.cookie || '');
-
-        if (url.pathname == '/favicon.ico') return res.end(favicon);
-        
-        console.log(req.socket.remoteAddress, `${url.pathname}${url.search}`);
-
-        if (url.pathname == '/auth') {
-            let state = url.searchParams.get('state') || `https://${config.web.hostname}`;
-            let valid = true;
-            try {
-                new URL(state)
-                valid = true;
-            } catch (err) {}
-            if (state == null || !valid) return end(400, errorPages[400]);
-            let code = url.searchParams.get('code');
-            if (code == null) return end(400, errorPages[400]);
-            let response = await fetch('https://discord.com/api/v10/oauth2/token', {
-                method: 'POST',
-                headers: {
-                    Authorization: `Basic ${btoa(`${config.discord.id}:${config.discord.secret}`)}`,
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: new URLSearchParams({
-                    grant_type: 'authorization_code',
-                    code,
-                    redirect_uri: authUrl
-                }).toString()
-            });
-            if (response.status == 400) return authRedirect(state);
-            else if (response.status != 200) {
-                console.log('Error fetching token:', await response.json());
-                end(500, `Discord API returned code ${response.status}`);
-                return;
-            }
-            let data = await response.json();
-            
-            let token = data.access_token;
-            response = await fetch('https://discord.com/api/v10/users/@me', {
-                headers: {
-                    Authorization: `Bearer ${token}`
-                }
-            });
-            if (response.status == 401) return authRedirect(state);
-            else if (response.status != 200) {
-                console.log('Error fetching user data:', await response.json());
-                end(500, `Discord API returned code ${response.status}`);
-                return;
-            }
-            let user = await response.json();
-            
-            res.appendHeader('set-cookie', `token=${createToken(JSON.stringify({ id: user.id, exp: Math.floor(Date.now() / 1000) + config.jwt.lifetime }))}`);
-            redirect(state);
-            return;
+        function end(code, body) {
+            if (code != null) res.statusCode = code;
+            res.end(typeof body == 'object' ? JSON.stringify(body) : body);
         }
 
-        if (url.pathname == '/register') {
-            return res.end(staticFiles['register.html']);
+        function redirect(location) {
+            res.setHeader('Location', location);
+            end(303);
         }
-        
-        let payload;
+
+        function authRedirect(state, location) {
+            state = state || url.href;
+            location = location || oauthUrl.href;
+            let redirectUrl = new URL(location);
+            redirectUrl.searchParams.set('state', state);
+            redirect(redirectUrl.href);
+        }
+
         try {
-            let token = parseToken(cookies.token);
-            if (!token.secret) return authRedirect();
-            payload = JSON.parse(token.payload);
-            if (Math.floor(Date.now() / 1000) >= payload.exp) return authRedirect();
-        } catch (err) {
-            return authRedirect();
-        }
+            let cookies = cookie.parseCookie(req.headers.cookie || '');
 
-        let user = await getUser(payload.id);
-        if (config.discord.registerBypass.includes(payload.id)) user = payload;
-        if (user == null) return authRedirect(null, registerUrl.href);
-        if (user.error) {
-            console.log('Error fetching db user:', user.error)
+            console.log(req.socket.remoteAddress, `${url.pathname}${url.search}`);
+
+            if (url.pathname == '/auth') {
+                let state = url.searchParams.get('state') || origin;
+                let valid = true;
+                try {
+                    new URL(state)
+                    valid = true;
+                } catch (err) {}
+                if (state == null || !valid) return end(400, errorPages[400]);
+                let code = url.searchParams.get('code');
+                if (code == null) return end(400, errorPages[400]);
+                let response = await fetch('https://discord.com/api/v10/oauth2/token', {
+                    method: 'POST',
+                    headers: {
+                        Authorization: `Basic ${btoa(`${config.discord.id}:${config.discord.secret}`)}`,
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    body: new URLSearchParams({
+                        grant_type: 'authorization_code',
+                        code,
+                        redirect_uri: authUrl
+                    }).toString()
+                });
+                if (response.status == 400) return authRedirect(state);
+                else if (response.status != 200) {
+                    console.log('Error fetching token:', await response.json());
+                    end(500, `Discord API returned code ${response.status}`);
+                    return;
+                }
+                let data = await response.json();
+                
+                let token = data.access_token;
+                response = await fetch('https://discord.com/api/v10/users/@me', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+                if (response.status == 401) return authRedirect(state);
+                else if (response.status != 200) {
+                    console.log('Error fetching user data:', await response.json());
+                    end(500, `Discord API returned code ${response.status}`);
+                    return;
+                }
+                let user = await response.json();
+                
+                res.appendHeader('set-cookie', `token=${createToken(JSON.stringify({ id: user.id, exp: Math.floor(Date.now() / 1000) + config.jwt.lifetime }))}`);
+                redirect(state);
+                return;
+            }
+            
+            let payload;
+            try {
+                let token = parseToken(cookies.token);
+                if (!token.secret) return authRedirect();
+                payload = JSON.parse(token.payload);
+                if (Math.floor(Date.now() / 1000) >= payload.exp) return authRedirect();
+            } catch (err) {
+                console.log(err)
+                return authRedirect();
+            }
+
+            let user = await getUser(payload.id);
+            if (config.discord.registerBypass.includes(payload.id)) user = payload;
+            if (user == null) return authRedirect(null, registerUrl.href);
+            if (user.error) {
+                console.log('Error fetching db user:', user.error)
+                end(500, errorPages[500]);
+                return;
+            }
+            let guildMember = await guild.members.fetch(user.id);
+            user.staff = false;
+            for (const role of config.discord.staffRoles) if (guildMember.roles.cache.get(role)) user.staff = true;
+            
+            let path = url.pathname.slice(1).split('/');
+            if (path[path.length - 1] == '') path = path.slice(0, -1);
+            if (path[0] == 'api') {
+                let endpoint = path[1];
+                let input = {
+                    req,
+                    res,
+                    end,
+                    url,
+                    user
+                }
+                if (api[endpoint]) {
+                    res.setHeader('Content-Type', 'application/json');
+                    let apiModule = api[endpoint];
+                    for (let option of apiModule.options || []) {
+                        let value = url.searchParams.get(option.name);
+                        if (option.required && value == null) return end(400, { error: `Missing required arg "${option.name}"` });
+
+                        if (value != null && option.accepts != null && !option.accepts.includes((typeof value == 'string' && option.caseInsensitive) ? value.toLowerCase() : value)) {
+                            res.statusCode = 400;
+                            res.end(JSON.stringify({ error: `"${value}" is not an accepted value for arg "${option.name}"` }));
+                            return;
+                        }
+                    }
+                    try {
+                        await apiModule.execute(input);
+                    } catch (err) {
+                        console.log(err);
+                        end(500, { error: 'Internal Error' });
+                    }
+                    return;
+                }
+            }
+
+            let page = staticFiles;
+            for (let i = 0; page != null && i < path.length; i++) page = page[path[i]] || page[`${path[i]}.html`];
+            if (!Buffer.isBuffer(page) && typeof page == 'object') page = page['index.html'];
+            if (page != null) return res.end(page);
+            
+            end(404, errorPages[404]);
+        } catch (err) {
+            console.error(err);
             end(500, errorPages[500]);
             return;
         }
-        let guildMember = await guild.members.fetch(user.id);
-        user.staff = false;
-        for (const role of config.discord.staffRoles) if (guildMember.roles.cache.get(role)) user.staff = true;
-        
-        let path = url.pathname.slice(1).split('/');
-        if (path[path.length - 1] == '') path = path.slice(0, -1);
-        if (path[0] == 'api') {
-            let endpoint = path[1];
-            let input = {
-                req,
-                res,
-                end,
-                url,
-                user
-            }
-            if (api[endpoint]) {
-                res.setHeader('Content-Type', 'application/json');
-                let apiModule = api[endpoint];
-                for (let option of apiModule.options || []) {
-                    let value = url.searchParams.get(option.name);
-                    if (option.required && value == null) return end(400, { error: `Missing required arg "${option.name}"` });
+    })
+}
 
-                    if (value != null && option.accepts != null && !option.accepts.includes((typeof value == 'string' && option.caseInsensitive) ? value.toLowerCase() : value)) {
-                        res.statusCode = 400;
-                        res.end(JSON.stringify({ error: `"${value}" is not an accepted value for arg "${option.name}"` }));
-                        return;
-                    }
-                }
-                try {
-                    await apiModule.execute(input);
-                } catch (err) {
-                    console.log(err);
-                    end(500, { error: 'Internal Error' });
-                }
-                return;
-            }
-        }
-
-        let page = staticFiles;
-        for (let i = 0; page != null && i < path.length; i++) page = page[path[i]] || page[`${path[i]}.html`];
-        if (!Buffer.isBuffer(page) && typeof page == 'object') page = page['index.html'];
-        if (page != null) return res.end(page);
-
-        end(404, errorPages[404])
-    } catch (err) {
-        console.error(err);
-        end(500, errorPages[500]);
-        return;
-    }
-}));
-
-server.listen(443);
+if (config.web.https) {
+    let server = http2.createSecureServer({
+        key: fs.readFileSync(config.web.privateKey),
+        cert: fs.readFileSync(config.web.certificate)
+    });
+    server.on('error', (err) => console.error(err));
+    server.on('request', handleRequest);
+    server.listen(config.web.port);
+} else http.createServer(handleRequest).listen(config.web.port);
