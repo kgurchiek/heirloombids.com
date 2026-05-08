0232ebd973055351a1548f668b4f1818522a37e3
Cornbread2100 <cornbread2100@cornbread2100.com>
2026-05-08 09:52:44 -0400

    typo fix

--- api/verify.js
+++ api/verify.js
@@ -22,8 +22,7 @@ export default {
 
         if (!user.staff) return end(403, { error: 'Staff Only', details: 'Only staff can verify signups' });
 
-        let signup;
-        ({ data: signup, error } = await supabase.from(config.supabase.tables.signups).update({ verified }).eq('signup_id', id).select('*'));
+        let { data: signup, error } = await supabase.from(config.supabase.tables.signups).update({ verified }).eq('signup_id', id).select('*');
         if (error) return end(500, { error: 'Error verifying signup', details: error.message });
         if (signup.length == 0) return end(400, { error: `Couldn't find signup with id "${id}"` });
         res.end(JSON.stringify(signup[0]));
